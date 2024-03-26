4c2af6189ba49d099bd81697841a8502cf41686a
lars-boesch <lars.boesch@gmail.com>
2024-03-26 14:02:48 +0100

    backend#18: first handler method for findme-location-riddles microservice.

--- findme-location-riddles/handler.py
+++ findme-location-riddles/handler.py
@@ -1,2 +1,37 @@
-def lambda_handler(event: dict, context) -> dict:
-    return {"body": "hi"}
+import sys
+
+sys.path.insert(0, "/var/task/.venv/lib/python3.12/site-packages")
+import os
+
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver, CORSConfig
+from aws_lambda_powertools.logging import correlation_paths
+from aws_lambda_powertools.utilities.typing import LambdaContext
+
+from findme.authorization import Authorizer
+
+tracer = Tracer()
+logger = Logger()
+
+cors_config = CORSConfig(allow_origin=os.environ.get("FRONTEND_ORIGIN"))
+app = APIGatewayRestResolver(cors=cors_config)
+
+authorizer = Authorizer(
+    auth0_domain=os.environ.get("AUTH0_DOMAIN"),
+    auth0_audience=os.environ.get("AUTH0_AUDIENCE"),
+)
+
+
+@app.post("/location-riddles")
+@tracer.capture_method
+@authorizer.requires_auth(app=app)
+def post_location_riddles():
+    print(app.current_event)
+    return {"message": "hello"}
+
+
+# You can continue to use other utilities just as before
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_REST)
+@tracer.capture_lambda_handler
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
