940472ae6f49cad8bea993473c8358a8c72a29c4
Jordi Küffer <jordikueffer@gmail.com>
2024-03-30 20:10:26 +0100

    backend #24: restructures location riddle table to use partition key and GSI

--- findme-location-riddles/src/LocationRiddleRepository.py
+++ findme-location-riddles/src/LocationRiddleRepository.py
@@ -1,26 +1,30 @@
 import boto3
-from src.LocationRiddle import LocationRiddle
-from botocore.exceptions import ClientError
-from boto3.dynamodb.conditions import Key
-from pydantic import ValidationError
 from aws_lambda_powertools.event_handler.exceptions import (
     BadRequestError,
     NotFoundError,
 )
+from botocore.exceptions import ClientError
+from pydantic import ValidationError
+from src.LocationRiddle import LocationRiddle
 
 
 class LocationRiddleRepository:
     def __init__(self):
-        self.dynamodb = boto3.resource('dynamodb', region_name='eu-central-2')
-        self.table = self.dynamodb.Table('locationRiddleTable')
+        self.dynamodb = boto3.resource("dynamodb", region_name="eu-central-2")
+        self.table = self.dynamodb.Table("locationRiddleTable")
 
     def write_location_riddle_to_db(self, user_id, location_riddle_id):
-        location_riddle_data = {"location_riddle_id": location_riddle_id, "user_id": user_id}
+        location_riddle_data = {
+            "location_riddle_id": location_riddle_id,
+            "user_id": user_id,
+        }
 
         try:
             location_riddle = LocationRiddle(**location_riddle_data)
         except ValidationError as e:
-            raise BadRequestError(f"unable to update location_riddle with provided parameters. {e}")
+            raise BadRequestError(
+                f"unable to update location_riddle with provided parameters. {e}"
+            )
 
         try:
             self.table.put_item(Item=location_riddle.dict())
@@ -31,21 +35,20 @@ class LocationRiddleRepository:
     def get_all_location_riddles(self):
         # ToDo: do the scanning by user_id here instead of LocationRiddlesService.get_location_riddles_for_user()
         response = self.table.scan()
-        data = response['Items']
+        data = response["Items"]
 
-        while 'LastEvaluatedKey' in response:
-            response = self.table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-            data.extend(response['Items'])
+        while "LastEvaluatedKey" in response:
+            response = self.table.scan(ExclusiveStartKey=response["LastEvaluatedKey"])
+            data.extend(response["Items"])
         return data
 
     def get_location_riddle_by_location_riddle_id_from_db(self, location_riddle_id):
-        # ToDo: fix query
-        response = self.table.query(
-            KeyConditionExpression=Key('location_riddle_id').eq(location_riddle_id)
-        )
+        response = self.table.get_item(Key={"location_riddle_id": location_riddle_id})
 
-        if 'Item' not in response:
-            raise NotFoundError(f"No location riddle with location_riddle_id: {location_riddle_id} found")
+        if "Item" not in response:
+            raise NotFoundError(
+                f"No location riddle with location_riddle_id: {location_riddle_id} found"
+            )
 
         # ToDo: create LocationRiddle instance from the response -> create new optional field image on dataclass
         return response["Item"]
