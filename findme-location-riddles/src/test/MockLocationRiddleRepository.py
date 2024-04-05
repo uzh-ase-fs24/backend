325398e6f9f5a8b5d4d973dd6527837e28d871eb
lars-boesch <lars.boesch@gmail.com>
2024-04-05 13:00:04 +0200

    backend #40: implement first test case for LocationRiddleService.

--- findme-location-riddles/src/test/MockLocationRiddleRepository.py
+++ findme-location-riddles/src/test/MockLocationRiddleRepository.py
@@ -1,15 +1,16 @@
 from src.base.AbstractDbRepository import AbstractDbRepository
+# from src.LocationRiddle import LocationRiddle
 
 
 class MockLocationRiddleRepository(AbstractDbRepository):
     def __init__(self):
         self.mock_data = {
-            "location_riddle_id": "mock_id",
+            "location_riddle_id": "mock_location_riddle_id",
             "user_id": "mock_user_id",
         }
 
-    def write_location_riddle_to_db(self, id, user_id):
-        return self.mock_data
+    def write_location_riddle_to_db(self, user_id, location_riddle_id):
+        pass
 
     def get_all_location_riddles_by_user_id(self, user_id):
         return [self.mock_data]
@@ -17,5 +18,5 @@ class MockLocationRiddleRepository(AbstractDbRepository):
     def get_location_riddle_by_location_riddle_id_from_db(self, id):
         return self.mock_data
 
-    def delete_location_riddle_from_db(self, id):
+    def delete_location_riddle_from_db(self, location_riddle_id):
         return {"message": "Mock delete successful"}
--- findme-location-riddles/tests/test_location_riddle_service.py
+++ findme-location-riddles/tests/test_location_riddle_service.py
@@ -1,25 +1,18 @@
 import unittest
-# from src.LocationRiddlesService import LocationRiddlesService
+from src.LocationRiddlesService import LocationRiddlesService
 from src.test.MockImageBucketRepository import MockImageBucketRepository
 from src.test.MockLocationRiddleRepository import MockLocationRiddleRepository
+
 
 class TestLocationRiddleService(unittest.TestCase):
     def setUp(self):
         self.image_bucket_repository = MockImageBucketRepository()
         self.location_riddle_repository = MockLocationRiddleRepository()
-        # self.location_riddles_service = LocationRiddlesService(self.location_riddle_repository, self.image_bucket_repository)
+        self.location_riddles_service = LocationRiddlesService(self.location_riddle_repository, self.image_bucket_repository)
 
-    def test_post_image_to_s3(self):
-        self.assertEqual(self.image_bucket_repository.post_image_to_s3("image", "key"),
+    def test_post_location_riddle(self):
+        self.assertEqual(self.location_riddles_service.post_location_riddle("mock_image_base64", "mock_user_id"),
                          {"message": "Mock image upload successful"})
-
-
-    def test_get_image_from_s3(self):
-        self.assertEqual(self.image_bucket_repository.get_image_from_s3("key"),
-                         {
-                             "image_base64": "mock_image_base64",
-                             "Content-Type": "image/png"
-                         })
 
 
 if __name__ == '__main__':
