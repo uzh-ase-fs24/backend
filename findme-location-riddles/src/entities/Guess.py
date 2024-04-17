aad6909badb54effd74275a5469de675dec80675
lars-boesch <lars.boesch@gmail.com>
2024-04-17 09:28:54 +0200

    do coordinate inputvalidation using pydantic.

--- findme-location-riddles/src/entities/Guess.py
+++ findme-location-riddles/src/entities/Guess.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, ValidationError
 from typing import List
 from decimal import Decimal
 
@@ -6,3 +6,13 @@ from decimal import Decimal
 class Guess(BaseModel):
     user_id: str
     guess: List[Decimal]
+
+    @field_validator('guess')
+    def validate_coordinates(cls, v):
+        if len(v) != 2:
+            raise ValidationError('Coordinate should contain 2 values: latitude and longitude')
+        if not (-90 <= v[0] <= 90):
+            raise ValidationError('Latitude should be between -90 and 90')
+        if not (-180 <= v[1] <= 180):
+            raise ValidationError('Longitude should be between -180 and 180')
+        return v
--- findme-location-riddles/src/entities/LocationRiddle.py
+++ findme-location-riddles/src/entities/LocationRiddle.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator, ValidationError
 from typing import List, Optional
 from decimal import Decimal
 
@@ -22,3 +22,13 @@ class LocationRiddle(BaseModel):
         super().__init__(**data)
         if self.ratings and isinstance(self.ratings[0], Rating):
             self.average_rating = sum(rating.rating for rating in self.ratings) / len(self.ratings)
+
+    @field_validator('location')
+    def validate_coordinates(cls, v):
+        if len(v) != 2:
+            raise ValidationError('Coordinate should contain 2 values: latitude and longitude')
+        if not (-90 <= v[0] <= 90):
+            raise ValidationError('Latitude should be between -90 and 90')
+        if not (-180 <= v[1] <= 180):
+            raise ValidationError('Longitude should be between -180 and 180')
+        return v
