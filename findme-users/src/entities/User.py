75b65340c6a68088ec1ab51db61eb9f7e75e10c9
lars-boesch <lars.boesch@gmail.com>
2024-04-17 11:24:30 +0200

    backend #57: add scores and average_score to user entity.

--- findme-users/src/entities/User.py
+++ findme-users/src/entities/User.py
@@ -1,4 +1,6 @@
+from typing import List, Optional
 from pydantic import BaseModel
+from src.entities.Score import Score
 
 
 class User(BaseModel):
@@ -6,3 +8,11 @@ class User(BaseModel):
     username: str
     first_name: str
     last_name: str
+    scores: List[Score] = []
+    average_score: Optional[float] = None
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        if self.scores and isinstance(self.scores[0], Score):
+            self.average_score = sum(score.score for score in self.scores) / len(self.scores)
+
