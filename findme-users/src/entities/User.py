740bcc508c5f5c13cce0db9d79b0e0bdf27a44a4
lars-boesch <lars.boesch@gmail.com>
2024-04-20 16:59:30 +0200

    backend #59: introduce UserDTO.

--- findme-users/src/entities/User.py
+++ findme-users/src/entities/User.py
@@ -8,13 +8,34 @@ class User(BaseModel):
     username: str
     first_name: str
     last_name: str
-    _scores: List[Score] = []
+    scores: List[Score] = []
     average_score: Optional[float] = None
 
     def __init__(self, **data):
         super().__init__(**data)
-        if self._scores and isinstance(self._scores[0], Score):
-            self.average_score = sum(score.score for score in self._scores) / len(self._scores)
+        if self.scores and isinstance(self.scores[0], Score):
+            self.average_score = sum(score.score for score in self.scores) / len(self.scores)
+
+    def to_dto(self):
+        return UserDTO(**self.dict())
+
+
+class UserDTO(BaseModel):
+    user_id: str
+    username: str
+    first_name: str
+    last_name: str
+    average_score: Optional[float] = None
+
+    def __init__(self, **data):
+        user = User(**data)
+        super().__init__(
+            user_id=user.user_id,
+            username=user.username,
+            first_name=user.first_name,
+            last_name=user.last_name,
+            average_score=user.average_score
+        )
 
 
 class UserPostDTO(BaseModel):
